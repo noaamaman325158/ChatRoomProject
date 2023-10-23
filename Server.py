aef604e499b1c4458dc9a1d1374d329ea4213c98
Noaa Maman <noaamaman325158@gmail.com>
2023-10-23 19:14:35 +0300

    Change the server infrastructure

--- Server.py
+++ Server.py
@@ -1,8 +1,14 @@
 import socket
 
+NUM_CURCURRENCY_CLIENTS = 5
 #Details about the socket connection between clients and server
-host = 'localhost'
+host = '127.0.0.1'
 port = 8080
+
+#Storage DS
+clients = {}
+addresses = {}
+
 
 #Create Socket obejct
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -10,14 +16,7 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #Bidn the server details
 sock.bind((host, port))
 
-sock.listen(1)
-print("The server is running and listening to client request: ")
-#accept some details about the client
-conn, address = sock.accept()
 
-message = "Hey there!"
-
-conn.send(message.encode())
-
-conn.close()
-
+if __name__ == '__main__':
+    sock.listen(NUM_CURCURRENCY_CLIENTS)
+    print("The server is running and listening to client requests...")
