03cd8e7fdf487cff5e2390dcc146384a4c83c9ca
Noaa Maman <noaamaman325158@gmail.com>
2023-10-24 04:12:06 +0300

    Implement some handle_client function for the accept_client_connection and some auxiliary function for performing some broadcast in our Chat Room Application.

--- Server.py
+++ Server.py
@@ -8,6 +8,8 @@ host = '127.0.0.1'
 port = 8080
 
 #Storage DS
+
+#The key is the associated connection with the client and the value is the name of the client
 clients = {}
 
 #The key is the connection of the client and the value is the address of the client
@@ -20,6 +22,35 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #Bidn the server details
 sock.bind((host, port))
 
+#Send Message to all the connected clients in out Chat Room
+def broadcast(msg, prefix=""):
+    for x in clients:
+        x.send(bytes(prefix, "utf-8")+msg)
+
+def handle_clients(conn, address):
+    name = conn.recv(1024).decode()
+    welcome = "Welcome "+ name + ". you can type #quit if you ever want to leave the Chat Room"
+    conn.recv(bytes(welcome, "utf-8"))
+    msg = name + "Has recently Joined the Chat Room"
+    broadcast(bytes(msg, "utf-8"))
+    #Store the associated client data inside the Client Dictionary
+    clients[conn] = name
+    #This loop going to run as well as the clients are send messages to the server
+    while True:
+        msg = conn.recv(1024)
+        #Check if the client type to quit
+        if msg != bytes("#quit", "utf-8"):
+            broadcast(msg, name + ":")
+        else:
+            conn.send(bytes("#quit", "utf-8"))
+            conn.close()
+            del clients[conn]
+            broadcast(bytes(name + " Has Left the Chat Room"))
+
+
+
+
+
 def accept_client_connections():
     #This loop going to run as well as the clients are send messages to the server
     while True:
@@ -29,6 +60,7 @@ def accept_client_connections():
         client_conn.send("Welcome to the chat room, Please type your name to continue".encode("utf-8"))
         #Store the data of the client inside our clients
         addresses[client_conn] = client_address
+        Thread(target=handle_clients, args=(client_conn,client_address)).start()
 
 
 
@@ -37,6 +69,7 @@ if __name__ == '__main__':
     sock.listen(NUM_CURCURRENCY_CLIENTS)
     print("The server is running and listening to client requests...")
 
+    #Thread will handle the concurrency-clients
     t1 = Thread(target=accept_client_connections)
     t1.start()
     t1.join()
