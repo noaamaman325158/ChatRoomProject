a26885c09c8d8e01cb4ac333cf7daf797c3707d7
Noaa Maman <noaamaman325158@gmail.com>
2023-10-26 00:58:32 +0300

    Here we establish some socket connection in the client segment and implement some threading functionality.

--- Client.py
+++ Client.py
@@ -2,7 +2,7 @@ import tkinter
 import socket
 from tkinter import *
 from threading import Thread
-
+from auxilary_data import PORT, HOST_IP
 
 #For creating a window via the tkinter module and init the value of the attributes
 window = Tk()
@@ -26,6 +26,29 @@ msg_list_box.pack()
 
 scroll_bar.pack(side="right", fill=Y)
 scroll_bar.pack()
+
+
+label = Label(window,text="Enter the message", fg="black", font="Aria",bg="grey")
+label.pack()
+
+entry_field = Entry(window,textvariable=my_message, fg="red", width=50)
+entry_field.pack()
+
+send_button = Button(window,text="Send", font="Aria", fg="White", command=send)
+send_button.pack()
+
+quit_button = Button(window, text="Quit", font="Aria", fg="White", command=on_closing)
+quit_button.pack()
+
+Host = HOST_IP
+Port = PORT
+
+#Establish some socket connection
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect((Host, Port))
+
+receive_messages_thread = Thread(target=receive)
+receive_messages_thread.start()
 
 
 
