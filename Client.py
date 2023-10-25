e0bc784583b011e572433cf5271c8526e29610ff
Noaa Maman <noaamaman325158@gmail.com>
2023-10-26 00:42:22 +0300

    Add some UI for the client side of the application and using the tkinter module for the GUI programming.

--- Client.py
+++ Client.py
@@ -1,19 +1,32 @@
+import tkinter
 import socket
-from auxilary_data import MAX_SIZE_MESSAGE
-
-host = 'localhost'
-port = 8080
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#Connect the server to the client
-sock.connect((host, port))
-
-#1024 bytes should be enough for message
-message = sock.recv(MAX_SIZE_MESSAGE)
-
-while message:
-    print("Message: ", message.decode())
-    message = sock.recv(MAX_SIZE_MESSAGE)
+from tkinter import *
+from threading import Thread
 
 
-sock.close()
+#For creating a window via the tkinter module and init the value of the attributes
+window = Tk()
+window.title("Chat Room App")
+window.configure(bg="pink")
+
+message_frame = Frame(window, height=100, width=100, bg="red")
+message_frame.pack()
+
+my_message = StringVar()
+my_message.set("")
+
+#The UI object Scroll Bar is located inside the message frame that we defined
+scroll_bar = Scrollbar(message_frame)
+
+#The UI object List Box is located inside the message frame
+msg_list_box = Listbox(message_frame,height="15", width="100", bg="red",yscrollcommand=scroll_bar.set)
+msg_list_box.pack(side="left", fill=BOTH)
+msg_list_box.pack()
+
+
+scroll_bar.pack(side="right", fill=Y)
+scroll_bar.pack()
+
+
+
+mainloop()
